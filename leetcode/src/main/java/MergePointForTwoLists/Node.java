d959ca5e26d2c1d7b726ec997e597a5d107c96a2
vivek92 <vsrivivek9@gmail.com>
2016-04-19 16:11:35 +0530

    merge point for two lists

---
+++ leetcode/src/main/java/MergePointForTwoLists/Node.java
@@ -0,0 +1,13 @@
+package MergePointForTwoLists;
+
+/**
+ * Created by vivek.vanga on 19/04/16.
+ */
+public class Node {
+    Node next;
+    String value;
+    Node( String i) {
+        this.next = null;
+        this.value = i;
+    }
+}
